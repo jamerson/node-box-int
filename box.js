12814c54141f79f6b47eaf92d08339d91979fb20
jamerson <jraposo@br.ibm.com>
2017-09-13 22:17:56 -0300

    back to normal

---
+++ box.js
@@ -0,0 +1,31 @@
+var passport = require('passport'),
+    BoxStrategy = require('passport-box').Strategy,
+    box_sdk = require('box-sdk'),
+    fs = require("fs"),
+    cfenv = require("cfenv");
+
+var vcapLocal = null;
+try {
+    vcapLocal = require("./vcap-local.json");
+}
+catch (e) {}
+
+var appEnvOpts = vcapLocal ? {vcap:vcapLocal} : {};
+var appEnv = cfenv.getAppEnv(appEnvOpts);
+
+var boxCreds = appEnv.getServiceCreds("box"),
+    box = box_sdk.Box();
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+});
+
+passport.use(new BoxStrategy({
+    clientID: boxCreds.clientId || boxCreds.client_id,
+    clientSecret: boxCreds.clientSecret || boxCreds.client_secret,
+    callbackURL: config.appURL(appEnv.port) + "/auth/box/callback"
+}, box.authenticate()));
